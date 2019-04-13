a0e17815c8398898d942460d8203d24a152abb7d
Julian Katz <juliankatz1991@gmail.com>
2019-04-13 14:18:11 -0700

    got a couple routes going

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
+// Modules
 const express = require('express')
-const server = express()
+const axios = require('axios')
+
+// Config
+require('dotenv').config()
 const port = 3000
 
+// Setup
+const server = express()
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: process.env.API_KEY,
+    language: 'en-US'
+  }
+})
+
 server.get('/', (req, res) => res.send('Hello World!'))
+
+server.get('/movie/popular', async (req, res) => {
+  console.log('POPULAR')
+  const response = await instance.get('/movie/popular', {
+    page: '1'
+  })
+
+  res.send(response.data.results)
+})
+
+server.get('/movie/:id', async (req, res) => {
+  const response = await instance.get(`/movie/${req.params.id}`)
+
+  if (response.data.status_code === 6) {
+    res.sendStatus(404)
+  } else {
+    res.send(response.data)
+  }
+})
 
 if (process.env.TEST !== 'true') {
   server.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- server.spec.js
+++ server.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const server = require('./server')
+const expect = require('chai').expect
 
 describe('/', () => {
   describe('GET', () => {
@@ -10,3 +11,40 @@ describe('/', () => {
     })
   })
 })
+
+describe('/movie/popular', () => {
+  describe('GET', () => {
+    it('returns a list of movies', done => {
+      request(server)
+        .get('/movie/popular')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.be.an('array')
+          expect(res.body).to.have.lengthOf(20)
+          done()
+        })
+    })
+  })
+})
+
+describe('/movie/someId', () => {
+  describe('GET', () => {
+    it('returns Hellboy for id 456740', done => {
+      request(server)
+        .get('/movie/456740')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.id).to.equal(456740)
+          expect(res.body.title).to.equal('Hellboy')
+          done()
+        })
+    })
+
+    it('returns a 404 for an id that does not exist', done => {
+      request(server)
+        .get('/movie/01910191019101910191019101')
+        .expect(404, done)
+    })
+  })
+})
+// describe('/search/movie')
