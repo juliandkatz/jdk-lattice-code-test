05f7108767e564e30d180344d99f6ef22775707b
Julian Katz <juliankatz1991@gmail.com>
2019-04-17 20:40:28 -0500

    implemented search

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Input } from 'semantic-ui-react'
 import Axios from 'axios'
 
 import MovieList from './MovieList.js'
@@ -13,24 +13,48 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      popularMovies: []
+      movies: [],
+      searchTerm: ''
     }
+
+    this.handleSearchInput = this.handleSearchInput.bind(this)
+    this.handleSearchClick = this.handleSearchClick.bind(this)
   }
 
   async componentDidMount () {
-    // make a call to the backend
+    // make a call to the backend to retrieve the popular movies
     const response = await backend.get('/movie/popular')
 
     // setState with the array of movies
-    this.setState({ popularMovies: response.data })
+    this.setState({ movies: response.data })
 
     console.log(response.data)
+  }
+
+  handleSearchInput (event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  async handleSearchClick () {
+    const response = await backend.get('/movie/search', {
+      params: {
+        query: this.state.searchTerm
+      }
+    })
+
+    this.setState({ movies: response.data })
   }
 
   render () {
     return (
       <Container>
-        <MovieList movies={this.state.popularMovies} />
+        <Input
+          onChange={this.handleSearchInput}
+          action={{ content: 'Search', onClick: this.handleSearchClick }}
+          placeholder='Search...'
+          value={this.state.searchTerm}
+        />
+        <MovieList movies={this.state.movies} />
       </Container>
     )
   }
--- client/src/MovieList.js
+++ client/src/MovieList.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react'
 import { Item, Rating } from 'semantic-ui-react'
 import './App.css'
 
-class MovieList extends Component {
-  constructor (props) {
-    super(props)
-  }
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/'
 
+class MovieList extends Component {
   renderListItem (movie) {
     return (
       <Item key={movie.id}>
-        <Item.Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} size='tiny' />
+        <Item.Image src={IMAGE_URL + movie.poster_path} size='tiny' />
         <Item.Content>
           <Item.Header>{movie.title}</Item.Header>
           <Item.Meta>
@@ -18,7 +16,11 @@ class MovieList extends Component {
           </Item.Meta>
           <Item.Description>
             <div className='small-text'>{movie.overview}</div>
-            <Rating icon='star' defaultRating={Math.round(movie.vote_average / 2)} maxRating={5} />
+            <Rating
+              icon='star'
+              defaultRating={Math.round(movie.vote_average / 2)}
+              maxRating={5}
+            />
           </Item.Description>
         </Item.Content>
       </Item>
@@ -27,9 +29,11 @@ class MovieList extends Component {
 
   render () {
     return (
-      <Item.Group divided>
-        { this.props.movies.map(movie => this.renderListItem(movie)) }
-      </Item.Group>
+      <div>
+        <Item.Group divided>
+          { this.props.movies.map(movie => this.renderListItem(movie)) }
+        </Item.Group>
+      </div>
     )
   }
 }
